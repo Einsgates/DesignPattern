bb11b9fec4d3ff00aa62e39c7b551988d664330c
Einsgate <597857000@qq.com>
2021-02-18 15:10:33 +0800

    cn

---
+++ 单例模式/src/SingletonPatternDemo.java
@@ -0,0 +1,14 @@
+public class SingletonPatternDemo {
+    public static void main(String[] args) {
+
+        //不合法的构造函数
+        //编译时错误：构造函数 SingleObject() 是不可见的
+        //SingleObject object = new SingleObject();
+
+        //获取唯一可用的对象
+        Singleton5 object = Singleton5.getInstance();
+
+        //显示消息
+        object.showMessage();
+    }
+}
