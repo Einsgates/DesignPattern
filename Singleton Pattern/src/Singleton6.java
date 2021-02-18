60c95a32f2b998ca486dfabfa2414109742f253b
Einsgate <597857000@qq.com>
2021-02-18 15:09:18 +0800

    SP

---
+++ Singleton Pattern/src/Singleton6.java
@@ -0,0 +1,9 @@
+public enum Singleton6 {
+    INSTANCE;
+    public void whatevermind() {
+    }
+    public void showMessage() {
+        System.out.println("Hello World!");
+        System.out.println("Singleton6");
+    }
+}
